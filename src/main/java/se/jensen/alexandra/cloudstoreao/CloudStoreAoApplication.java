fa2022c1a5c796f1359cd986b9390b45decba7b9
AlexandraO93 <alexandra.olsson93@hotmail.com>
2026-04-30 13:01:53 +0200

    chore: initial project metadata and root configuration

---
+++ src/main/java/se/jensen/alexandra/cloudstoreao/CloudStoreAoApplication.java
@@ -0,0 +1,13 @@
+package se.jensen.alexandra.cloudstoreao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudStoreAoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudStoreAoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/jensen/alexandra/cloudstoreao/CloudStoreAoApplicationTests.java
@@ -0,0 +1,13 @@
+package se.jensen.alexandra.cloudstoreao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudStoreAoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
